5de636e48c7c99b67f899049fd3895e28c744561
Aman Chaudhary <amanchaudhary4510@gmail.com>
2026-02-20 01:20:44 +0530

    Secure firebase admin setup using env variable

--- config/firebase.js
+++ config/firebase.js
@@ -1,8 +1,17 @@
 const admin = require("firebase-admin");
-const serviceAccount = require("./serviceAccountKey.json");
+
+let serviceAccount;
+
+if (process.env.FIREBASE_SERVICE_ACCOUNT) {
+    // Production (Render)
+    serviceAccount = JSON.parse(process.env.FIREBASE_SERVICE_ACCOUNT);
+} else {
+    // Local development (optional)
+    serviceAccount = require("./serviceAccountKey.json");
+}
 
 admin.initializeApp({
-    credential: admin.credential.cert(serviceAccount)
+    credential: admin.credential.cert(serviceAccount),
 });
 
 module.exports = admin;
