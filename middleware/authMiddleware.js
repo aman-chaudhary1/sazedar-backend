6f42adbe90080a00a722eed96d32805a0e95f356
Aman Chaudhary <amanchaudhary4510@gmail.com>
2026-04-27 16:11:34 +0530

    comit

--- middleware/authMiddleware.js
+++ middleware/authMiddleware.js
@@ -5,7 +5,7 @@ module.exports = async (req, res, next) => {
   try {
     const token = req.headers.authorization?.split(' ')[1];
     if (!token) {
-      return res.status(401).json({ success: false, message: 'Unauthorized' });
+      return res.status(401).json({ success: false, message: 'Unauthorized: No token provided' });
     }
 
     const decoded = jwt.verify(token, process.env.JWT_SECRET || 'your-secret-key');
